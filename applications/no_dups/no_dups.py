69649c0584e7a479fd8beef88388fe9ec35079b8
jarparrish <jarparrish@gmail.com>
2020-11-03 18:41:03 -0800

    no_dups passing tests

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,11 +1,32 @@
 def no_dups(s):
     # Your code here
+    str_dictionary = {}
+    s = s.split()
+    ans_str = ""
 
+    for word in s: 
+        if word in str_dictionary:
+            pass
+        else:
+            str_dictionary[word] = 1
+    str_keys = str_dictionary.keys()
+    print(len(str_keys))
+    key_len = len(str_keys)
+    count = 1 
+    for key in str_keys:
+        
+        print(key)
+        if count != key_len:
+            ans_str += (key + " ")
+            count += 1
+        else:
+            ans_str += key
+    return ans_str
 
 
 if __name__ == "__main__":
     print(no_dups(""))
-    print(no_dups("hello"))
+    print(len(no_dups("hello")))
     print(no_dups("hello hello"))
-    print(no_dups("cats dogs fish cats dogs"))
+    print(len(no_dups("cats dogs fish cats dogs")))
     print(no_dups("spam spam spam eggs spam sausage spam spam and spam"))

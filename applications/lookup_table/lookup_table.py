44dba5e4aaa490a04abf2c4db6c8912ebd86c810
jarparrish <jarparrish@gmail.com>
2020-11-03 16:04:58 -0800

    second implementation that pushes to cache as function runs implemented

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -22,7 +22,7 @@ Else, store x,y values in cashe as tuple, run function and return value
 
     return v """
 
-def cache_builder():
+""" def cache_builder():
     cache = {}
     def computation(x, y):
         v = math.pow(x, y)
@@ -38,17 +38,31 @@ def cache_builder():
     
     return cache
 
-cache = cache_builder()
+cache = cache_builder() """
+
+cache = {}
 
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-   
+    def computation(x, y):
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
+
+        return v
+
+    if (x, y) in cache:
+        return cache[(x, y)]
+    else:
+        cache[(x, y)] = computation(x, y)
+        return cache[(x, y)]
     
     
-    return cache[(x, y)]
+    
 
 # Do not modify below this line!
 
